28aa1b6acc8208e8ca43a172d16bb8a69bdf1069
Ruslan Kucherenko <fataler69@yandex.ru>
2020-05-27 18:27:42 +0300

    Updated gitignore

--- Assets/Scripts/SensitiveData.cs
+++
@@ -1,8 +0,0 @@
-﻿//that must me hidden
-public static class SensitiveData
-{
-    public const string ANDROIDADMOBID = "3494289";
-    public const string IOSADMOBID = "3494288";
-    public const string UNITYREWARDEDVIDEO = "ca-app-pub-8270143772782700/1036471962";
-    public const string UNITYINTERSTITIALVIDEO = "ca-app-pub-3940256099942544/1033173712";
-}
